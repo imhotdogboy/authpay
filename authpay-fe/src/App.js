c759307b91795b6a09cc5e57e33a334ddeac55b3
geee3 <c1as2icblu3@gmail.com>
2022-05-31 22:28:54 +0900

    ✨ Build SubmitFileData

--- authpay-fe/src/App.js
+++ authpay-fe/src/App.js
@@ -1,23 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import Axios from 'axios';
 
-function App() {
+const App = () => {
+  let formData = new FormData();
+
+  const onFileChange = (e) => {
+    console.log(e.target.files[0])
+    if(e.target && e.target.files[0]) {
+      formData.append('files', e.target.files[0]);
+    }
+  }
+
+  const SubmitFileData = () => {
+    Axios.post(
+      'http://127.0.0.1:8000/uploadfiles',
+      formData, {headers: {'Content-Type': 'multipart/form-data'}}
+    )
+    .then(res => {
+      console.log(res);
+    })
+    .catch(error => {
+      console.log(error);
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <input type="file" name="file_upload" onChange={onFileChange}/>
+      </div>
+      <div>
+        <button onClick={SubmitFileData}>파일 업로드</button>
+      </div>
     </div>
   );
 }

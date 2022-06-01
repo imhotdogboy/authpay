a6c131ee391995c05c6498f73e45850160b85e6c
geee3 <c1as2icblu3@gmail.com>
2022-06-02 04:04:28 +0900

    💄 Improve button UI design

--- authpay-fe/src/FileUpload/FileUpload.jsx
+++ authpay-fe/src/FileUpload/FileUpload.jsx
@@ -1,15 +1,25 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
 import React from 'react'
+import './FileUpload.scss'
 
 const FileUpload = ({files, setFiles}) => {
     const uploadHandler = () => {}
     return (
-        <>
-            <div className="file-card">
-                <div className="file-inputs">
-                    <input type="file" />
-                </div>
+        <div className="file-card">
+            <div className="file-inputs">
+                <input type="file" />
+                <button>
+                    <i>
+                        <FontAwesomeIcon icon={faPlus} />
+                    </i>
+                    Upload
+                </button>
             </div>
-        </>
+
+            <p className="main">파일 형식</p>
+            <p className="info">MP4, JPG, PNG</p>
+        </div>
     )
 }
 

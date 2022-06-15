cab5c7cdf965c6a4da8ce3749eae72cb4e4219a6
geee3 <c1as2icblu3@gmail.com>
2022-06-15 18:45:36 +0900

    ✨ Build onFileDrop

--- authpay-fe/src/FileUpload/FileUpload.jsx
+++ authpay-fe/src/FileUpload/FileUpload.jsx
@@ -1,52 +1,79 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlus } from '@fortawesome/free-solid-svg-icons'
-import React from 'react'
+import React, { useRef, useState } from 'react';
 import Axios from 'axios';
-import './FileUpload.scss'
+import './FileUpload.css';
+
+import { ImageConfig } from '../ImageConfig';
+import uploadImage from '../assets/cloud-upload-regular-240.png';
 
 const FileUpload = ({files, setFiles, removeFile}) => {
-    const uploadHandler = (event) => {
-        const file = event.target.files[0];
-        file.isUploading = true;
-        setFiles([...files, file])
+    const wrapperRef = useRef(null);
+    const [fileList, setFileList] = useState([]);
 
-        const formData = new FormData();
-        formData.append(
-            file.name,
-            file,
-            file.name
-        )
+    const onDragEnter = () => wrapperRef.current.classList.add('dragover');
+    const onDragLeave = () => wrapperRef.current.classList.remove('dragover');
+    const onDrop = () => wrapperRef.current.classList.remove('dragover');
 
-        Axios.post(
-            'http://127.0.0.1:8000/uploadfiles',
-            formData, {headers: {'Content-Type': 'multipart/form-data'}}
-        )
-        .then(res => {
-            file.isUploading = false;
+    const onFileDrop = (event) => {
+        const newFile = event.target.files[0];
+        if (newFile) {
+            const updatedList = [...fileList, newFile];
+            setFileList(updatedList);
+
+            const file = event.target.files[0];
+            file.isUploading = true;
             setFiles([...files, file])
-        })
-        .catch(error => {
-            console.error(error)
-            removeFile(file.name)
-        })
+
+            const formData = new FormData();
+            formData.append('files', file);
+
+            Axios.post(
+                'http://127.0.0.1:8000/uploadfiles',
+                formData, {headers: {'Content-Type': 'multipart/form-data'}}
+            )
+            .then(res => {
+                file.isUploading = false;
+                setFiles([...files, file])
+            })
+            .catch(error => {
+                console.error(error)
+                removeFile(file.name)
+            })
+        }
     }
 
     return (
-        <div className="file-card">
-            <div className="file-inputs">
-                <input type="file" onChange={uploadHandler}/>
-                <button>
-                    <i>
-                        <FontAwesomeIcon icon={faPlus} />
-                    </i>
-                    Upload
-                </button>
-            </div>
-
-            <p className="main">파일 형식</p>
-            <p className="info">MP4, JPG, PNG</p>
+        <div>
+            <div
+                ref={wrapperRef}
+                className="drop-file-input"
+                onDragEnter={onDragEnter}
+                onDragLeave={onDragLeave}
+                onDrop={onDrop}
+            >
+                <div className="drop-file-input-label">
+                    <image src={uploadImage} alt="" />
+                    <p>Drag & Drop your files here</p>
+                </div>
+                <input type="file" value="" onChange={onFileDrop} />
+            </div> {
+                fileList.length > 0 ? (
+                    <div className="drop-file-preview">
+                        <p className="drop-file-preview-title">Upload Complete</p> {
+                            fileList.map((item, index) => (
+                                <div key={index} className="drop-file-preview-item">
+                                    <img src={ImageConfig[item.type.split('/')[1]] || ImageConfig['default']} alt="" />
+                                    <div className="drop-file-preview-item-info">
+                                        <p>{item.name}</p>
+                                        <p>{item.size}B</p>
+                                    </div>
+                                </div>
+                            ))
+                        }
+                    </div>
+                ) : null
+            }
         </div>
-    )
+    );
 }
 
 export default FileUpload
---
+++ authpay-fe/src/ImageConfig.js
@@ -0,0 +1,9 @@
+import fileDefault from './assets/file-blank-solid-240.png';
+import filePng from './assets/file-png-solid-240.png';
+import fileMp4 from './assets/file-mp4-solid-240.png';
+
+export const ImageConfig = {
+    default: fileDefault,
+    png: filePng,
+    mp4: fileMp4
+}
